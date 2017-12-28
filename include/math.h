a72efc8a506f50f5b1f259afddcb51dbabf3df54
Tejas Sharma <sharmatejas0@gmail.com>
2017-12-28 10:16:48 -0500

    work so far

--- include/math.h
+++ include/math.h
@@ -1,10 +1,9 @@
 #include <set>
 namespace ts 
 {
-  int gcd(const int a, const int b);
+  int gcd(int a, int b);
   int raise(const int base, const int power);
   std::shared_ptr<std::vector<int> > prime_factors(const int num);
   std::shared_ptr<std::vector<int> > primes(const int max);
   bool is_prime(const int num);
-
 }
--- src/math.cpp
+++ src/math.cpp
@@ -1,1 +1,17 @@
 #include "math.h"
+
+namespace ts 
+{
+  // Euclid's Algorithm i.e. gcd(a, b) == gcd(b, a % b)
+  int gcd(int a, int b)
+  {
+    while (b != 0)
+    {
+      auto remainder = a % b;
+      a = b;
+      b = remainder;
+      remainder = a % b;
+    }
+    return b;
+  }
+}
