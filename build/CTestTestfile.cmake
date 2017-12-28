262efb3658b1c88f78e524fcdc45811b268610d9
Tejas Sharma <sharmatejas0@gmail.com>
2017-12-27 19:06:00 -0500

    removing build directory

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tsharma/src/math
-# Build directory: /Users/tsharma/src/math/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
